9e74f17820256f4f6584bca32b7f97df66952678
swaraj150 <swaraj.andhale@vit.edu.in>
2026-02-19 23:38:17 +0530

    add: add validations for objective and key-result dtos

--- backend_v3/src/key-result/dto/key-result.dto.ts
+++ backend_v3/src/key-result/dto/key-result.dto.ts
@@ -1,27 +1,75 @@
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Min,
+} from 'class-validator';
+
 export class CreateKeyResultDto {
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsNumber()
+  @Min(0)
   currentValue: number;
+
+  @IsNumber()
+  @Min(0)
   targetValue: number;
+
+  @IsString()
   metricType: string;
+
+  @IsUUID()
   objectiveId: string;
 }
 
 export class UpdateKeyResultDto {
+  @IsUUID()
   id: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   description?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
   currentValue?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
   targetValue?: number;
+
+  @IsOptional()
+  @IsString()
   metricType?: string;
+
+  @IsUUID()
   objectiveId: string;
 }
 
 export class UpdateCurrentValueDto {
+  @IsUUID()
   id: string;
+
+  @IsNumber()
+  @Min(0)
   currentValue: number;
+
+  @IsUUID()
   objectiveId: string;
 }
 
 export class DeleteKeyResultsDto {
+  @IsArray()
   keyResultsToDelete: string[];
+  @IsUUID()
   objectiveId: string;
 }
--- backend_v3/src/objective/dto/objective.dto.ts
+++ backend_v3/src/objective/dto/objective.dto.ts
@@ -1,12 +1,19 @@
 import { CreateKeyResultDto } from '../../key-result/dto/key-result.dto';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class CreateObjectiveDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
   keyResults: Omit<CreateKeyResultDto, 'objectiveId'>[];
 }
 
 export class UpdateObjectiveDto {
+  @IsUUID()
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
   title: string;
 }
 export class GenerateObjectiveDto {
